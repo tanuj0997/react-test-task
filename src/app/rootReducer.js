92085eb0a2a90695e3c9f3eab9ad40755560f4c7
Tanuj <tanuj@gmail.com>
2020-04-10 01:19:12 +0530

    Added reducer and saga in root

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import reports from '../containers/ReportsScreen/reducer';
+import integration from '../containers/IntegrationScreen/reducer';
+import notifications from '../containers/Notifications/reducer';
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
+  reports,
+  notifications,
+  integration,
 });
 
 export default createRootReducer;
--- src/app/rootSaga.js
+++ src/app/rootSaga.js
@@ -1,9 +1,13 @@
 import createSagaMiddleware from 'redux-saga';
 import { all, fork } from 'redux-saga/effects';
+import reportsSaga from '../containers/ReportsScreen/saga';
+import integrationSaga from '../containers/IntegrationScreen/saga';
 
 function* rootSaga() {
   yield all(
     [
+      fork(reportsSaga),
+      fork(integrationSaga),
     ]
   );
 }
