b18b5e8f89ed07f466e4474e305ce745d8c97bed
Tanuj <tanuj@gmail.com>
2020-04-10 01:16:14 +0530

    Added loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+
+const Loader = ({ loading }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Backdrop className={classes.backdrop} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+  );
+}
+
+Loader.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+export default Loader;
